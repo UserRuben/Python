8324892cd77ff47927259a367592666ffbb2fb00
UserRuben <ruben-aristides123@hotmail.com>
2025-03-18 10:28:26 -0600

    Ejercicio Python #2

---
+++ Python_2/Hello_DevOps.py
@@ -0,0 +1,8 @@
+import datetime
+
+# Mostrar fecha y hora actual
+print("Fecha y hora actual: ", datetime.datetime.now())
+# Solicitar el nombre del usuario
+nombre = input("¿Cual es tu nombre? ")
+# Mostrar mensaje de bienvenida
+print("Hola ",nombre," ¿Quieres aprender Python?")
